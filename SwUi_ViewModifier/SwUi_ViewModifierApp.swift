5d56911e7cc18a36dc1e70a7553a45c1c11c1300
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-08-15 19:22:39 -0300

    Initial Commit

---
+++ SwUi_ViewModifier/SwUi_ViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_ViewModifierApp.swift
+//  SwUi_ViewModifier
+//
+//  Created by Marcelo Sampaio on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_ViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
